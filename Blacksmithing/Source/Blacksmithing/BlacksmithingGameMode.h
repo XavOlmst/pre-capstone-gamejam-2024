60f5a0c2cbad7b81eb3466bfaeccbdde76ff3307
Hannah Fasco <hannah.fasco@mymail.champlain.edu>
2024-07-31 15:19:29 -0400

    template + gitignore

---
+++ Blacksmithing/Source/Blacksmithing/BlacksmithingGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlacksmithingGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABlacksmithingGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABlacksmithingGameMode();
+};
+
+
+
